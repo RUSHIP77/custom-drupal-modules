37a0473374f4233f5ffd5ac2be12312809424068
RUSHIP77 <59787710+RUSHIP77@users.noreply.github.com>
2023-06-23 08:32:34 +0100

    Update SampleGetRestResource.php
    i removed extra testing code . Still one example is left in the comments the car one for understanging the load() method

--- sample_rest_resource/src/Plugin/rest/Resource/SampleGetRestResource.php
+++ sample_rest_resource/src/Plugin/rest/Resource/SampleGetRestResource.php
@@ -30,21 +30,13 @@ class SampleGetRestResource extends ResourceBase
     public function get()
     {
         // Implementing our custom REST Resource here.
-        // Use currently logged user after passing authentication and validating the access of term list.
-        // if (!$this->loggedUser->hasPermission('access content')) throw new AccessDeniedHttpException();
-
         // $car_id = 'car';
         // $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($car_id);
-        $taxtermsdata = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadMultiple();
-        // foreach ($terms as $term) {
-        //     $term_res[] = array(
-        //         'id' => $term->tid,
-        //         'name' => $term->name,
-        //     );
-        // }
 
+
+       
+        $taxtermsdata = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadMultiple();
         $response = new ResourceResponse($taxtermsdata);
-     
         return $response;
     }
 }
